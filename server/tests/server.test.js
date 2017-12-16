17fe8f3d9bce4d567ab7370b9ab53d5fb6a7ba0b
Adam Karlsten <adamkarlsten@gmail.com>
2017-12-16 19:43:26 +0100

    Added tests for /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const request = require('supertest')
 const expect = require('expect')
+const {ObjectID} = require('mongodb')
 
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }]
 
@@ -73,5 +76,34 @@ describe('GET /todos', () => {
 })
 
 describe('GET /todos/:id', () => {
-  it()
+  it('should get the correct todo', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe('First test todo')
+      })
+      .end(done)
+  })
+
+  it('should return a 404 if todo not found', (done) => {
+    var id = new ObjectID().toHexString()
+    request(app)
+      .get(`/todos/${id}`)
+      .expect(404)
+      .expect((res) => {
+        expect(res.body.errormessage).toBe(`Unable to find TODO with id: ${id}`)
+      })
+      .end(done)
+  })
+
+  it('should return a 400 if the ObjectID is invalid', (done) => {
+    request(app)
+      .get('/todos/INVALID')
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.errormessage).toBe('Invalid ID')
+      })
+      .end(done)
+  })
 })
