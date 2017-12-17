86b3d56b7d2d382feef02672b9717a356a6c8e0b
Adam Karlsten <adamkarlsten@gmail.com>
2017-12-17 17:01:15 +0100

    added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var {ObjectID} = require('mongodb')
+const _ = require('lodash')
+const express = require('express')
+const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
@@ -73,6 +74,35 @@ app.delete('/todos/:id', (req, res) => {
       message: `Successfully deleted TODO with id: ${id}`,
       todo
     })
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id
+  var body = _.pick(req.body, ['text', 'completed'])
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(400).send({
+      errormessage: 'Invalid ID'
+    })
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false,
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send({
+        errormessage: `Unable to find TODO with id: ${id}`
+      })
+    }
+    res.send({todo})
   }).catch((e) => {
     res.status(400).send()
   })
