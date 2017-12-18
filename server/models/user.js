2cdf07e17c003062bda18970f5f262c7763f0778
Adam Karlsten <adamkarlsten@gmail.com>
2017-12-19 00:02:53 +0100

    added authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -41,7 +41,7 @@ UserSchema.methods.toJSON = function () {
 
   return _.pick(userObject, ['_id', 'email'])
 }
-
+//svårt!!
 UserSchema.methods.generateAuthToken = function () {
   var user = this
   var access = 'auth'
@@ -50,6 +50,23 @@ UserSchema.methods.generateAuthToken = function () {
   user.tokens.push({access, token})
   return user.save().then(() => {
     return token
+  })
+}
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this
+  var decoded
+
+  try {
+    decoded = jwt.verify(token, 'abc123')
+  } catch (e) {
+    return Promise.reject()
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   })
 }
 
